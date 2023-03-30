6b670591beea502ff76a528198bccb73ddbfda8e
karapenning <128439106+karapenning@users.noreply.github.com>
2023-03-29 21:11:18 -0500

    start of main page actions
    changes for now, making sure merging correctly

--- project-invyte/src/App.js
+++ project-invyte/src/App.js
@@ -1,17 +1,35 @@
+import React from "react";
+import { Button, TextField } from '@mui/material';
 import logo from './logo-icon.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Let's plan a party!
-        </p>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHost: false,
+      roomCode: null,
+    };
+  }
 
-export default App;
+  handleClick = () => {
+    this.setState({
+      isHost: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Let's plan a party!
+          </p>
+          <TextField id="roomCodeInput" label="Enter Room Code Here" variant="outlined" />
+          <Button id="" variant="contained">Start a New Room</Button>
+        </header>
+      </div>
+    );
+  }
+}
