3d651ee57165487fbef3b1863791a23a741f15f0
R3clusive <skynewberryler@outlook.com>
2023-04-02 11:09:42 -0500

    this commit is to learn what eject will do

--- project-invyte/src/components/EntryPage/EntryPage.js
+++ project-invyte/src/components/EntryPage/EntryPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, TextField } from '@mui/material';
 import logo from '../../logo-icon.png';
 import './EntryPage.css';
+import QRImage from "react-qr-image";
 
 const EntryPage = () => {
     const [roomCode, setRoomCode] = React.useState('');
@@ -9,13 +10,33 @@ const EntryPage = () => {
     return (
       <div className="EntryPage">
         <div className="roomCodeInput">
-            <TextField id="roomCodeInput" label="Enter Room Code Here" variant="outlined" />
+            <TextField id="roomCodeInput" label="Create a new room" variant="outlined" />
         </div>
+        <>
+      <QRImage text="hello" />
+      <QRImage text="hello" color="red" />
+      <QRImage text="hello" color="white" background="#111" />
+      <QRImage>hello</QRImage>
+    </>
         <div>
-            <Button id="" variant="contained">Start a New Room</Button>
+            <Button id="CreateRoomBoi"   
+            onClick={() => {
+
+
+              var cryptoRandomString = "lksjdga;oi"
+
+
+              alert(cryptoRandomString)
+            }}            
+            variant="contained">Start a New Room</Button>
         </div>
       </div>
+      
     );
 };
+
+
+
+
 
 export default EntryPage;
--- project-invyte/src/index.js
+++ project-invyte/src/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
