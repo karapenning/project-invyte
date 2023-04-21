9ec733de62c70a8c3ee3320648148396d5ccc036
karapenning <128439106+karapenning@users.noreply.github.com>
2023-04-20 19:20:03 -0500

    input form for add new event
    - added state
    - added input fields that save clientEvent
    - changed interface from Event to ClientEvent to avoid confusion

--- src/components/Board_New/Board.tsx
+++ src/components/Board_New/Board.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Button, Card, Divider, Paper, TextField } from '@mui/material';
+import { useState } from 'react';
+import { Button, Card, CardContent, FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, TextField } from '@mui/material';
 import './Board.css';
 
 type BoardProps = {
     boardId: string;
 };
 
-export interface Event {
+export interface ClientEvent {
     title: string;
     address?: string;
     multiDay?: boolean;
@@ -14,47 +15,100 @@ export interface Event {
     dateEnd?: Date;
 }
 
-const sampleEvent = {
-    title: "Dinner Party at Abby's",
-    address: "1516 Strawberry Lane",
-    multiDay: false,
-    dateStart: new Date('April 13, 2022 19:30:00'),
-    dateEnd: new Date('April 13, 2022 22:30:00'),
-}
+export const New_Board = ({ boardId }: BoardProps) => {
+    const [clientEvent, setClientEvent] = useState<ClientEvent>({title: '', multiDay: false});
+    
+    const sampleEvent = {
+        title: "Dinner Party at Abby's",
+        address: "1516 Strawberry Lane",
+        multiDay: false,
+        dateStart: new Date('April 13, 2022 19:30:00'),
+        dateEnd: new Date('April 14, 2022 22:30:00'),
+    }
 
-export const New_Board = ({ boardId }: BoardProps) => (
-    <Paper className='Board'>
-        <div className="Board-main">
-        <TextField  helperText="Input your Event Title" required value={sampleEvent.title}  variant='standard' > </TextField>
-            <Divider />
+    const onInputChange = (id: string, value: any) => {
+        // let x = e;
+        // let y = e.target;
+        let x = value;
+        setClientEvent({...clientEvent, [id]: value});
+    }
+    
+    const clientEventDateTypeChanged = (e: React.FormEvent<HTMLInputElement>) => {
+        //if (e && typeof e.target.value === "string") {
+        //    if (e.target.value.toLowerCase() === "true")
+        //        sampleEvent.multiDay = true;
+        //    else 
+        //        sampleEvent.multiDay = false;
+        //}
+        let x = e;
+    };
+
+    return (
+        <Paper className='Board'>
             <Card>
-                <div>
-                    <b>Address</b>
-                    <p>{sampleEvent.address}</p>
-                </div>
-                <div>
-                    <b>ID</b>
-                    <p>{boardId}</p>
-                </div>
-                <div>
-                    {sampleEvent.multiDay === true ? (
-                        <>
-                            <b>Dates</b>
-                            <p>{sampleEvent.dateStart.toDateString()} - {sampleEvent.dateEnd.toDateString()}</p>
-                        </>
-                    ) : (
-                        <>
-                            <b>Date</b>
-                            <p>{sampleEvent.dateStart.toDateString()}</p>
-                            <p>{sampleEvent.dateStart.toLocaleTimeString()} - {sampleEvent.dateEnd.toLocaleTimeString()}</p>
-                        </>
-                    )}
-                </div>
+                <CardContent>
+                    <div>
+                    <TextField
+                        id='title'
+                        variant="outlined"
+                        label="Event Name"
+                        value={clientEvent.title}
+                        required
+                        onChange={(e)=>onInputChange('title', e.target.value)}
+                    />
+                    </div>
+                    <div>
+                        <TextField
+                            id='address'
+                            variant="outlined"
+                            label="Address"
+                            value={clientEvent.address}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            variant="outlined"
+                            label="ID"
+                            value={boardId}
+                            disabled
+                        />
+                    </div>
+                    <div>
+                        <FormControl>
+                            <FormLabel>Event Type</FormLabel>
+                            <RadioGroup row defaultValue={false} onChange={clientEventDateTypeChanged}>
+                                <FormControlLabel value={false} label='Single Day' control={<Radio />}/>
+                                <FormControlLabel value={true} label='Multiple Days' control={<Radio />}/>
+                            </RadioGroup>
+                        </FormControl>
+                        {/* <div>
+                            <TextField
+                                variant="outlined"
+                                label="Event Date"
+                                type="date"
+                                defaultValue={new Date().toLocaleString() + ""}
+                                value={sampleEvent.dateStart.toDateString()}
+                            />
+                        </div> */}
+                        {clientEvent && clientEvent.multiDay === true ? (
+                            <>
+                                <b>Dates</b>
+                                <p>{sampleEvent.dateStart.toDateString()} - {sampleEvent.dateEnd.toDateString()}</p>
+                            </>
+                        ) : (
+                            <>
+                                <b>Date</b>
+                                <p>{sampleEvent.dateStart.toDateString()}</p>
+                                <p>{sampleEvent.dateStart.toLocaleTimeString()} - {sampleEvent.dateEnd.toLocaleTimeString()}</p>
+                            </>
+                        )}
+                    </div>
+                </CardContent>
             </Card>
-        </div>
-        <Button onClick={() => {SavetoDb();}}>Save to DB</Button>
-    </Paper>
-)
+            <Button onClick={() => {SavetoDb();}}>Save to DB</Button>
+        </Paper>
+    );
+}
 
 
 function SavetoDb() {
@@ -62,4 +116,3 @@ function SavetoDb() {
 
 
 }
-
